5b76e433cc999d22fb9eb45a2846b8869938a872
Duc <thienductranhuu2784@gmail.com>
2025-01-16 12:52:29 -0500

    turning a class into a controller

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,11 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+    public String getHomePage() {
+        return "controller";
+    }
+}
