352300a6f7c425e5d2644f293c867f6af4f40a79
Duc <thienductranhuu2784@gmail.com>
2025-01-21 18:56:01 -0500

    MVC and Restful api models

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,11 +1,34 @@
 package vn.hoidanit.laptopshop.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
+import vn.hoidanit.laptopshop.services.UserService;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//MVC pattern
 @Controller
 public class UserController {
 
+    @RequestMapping("/")
     public String getHomePage() {
-        return "controller";
+        return "duc.html";
     }
 }
+// @RestController
+// public class UserController {
+
+// // Dependency Injection pattern
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping()
+// public String getHomePage() {
+// return "duc.html";
+// }
+// }
---
+++ src/main/java/vn/hoidanit/laptopshop/services/UserService.java
@@ -0,0 +1,10 @@
+package vn.hoidanit.laptopshop.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public String handleHello() {
+        return "hello";
+    }
+}
