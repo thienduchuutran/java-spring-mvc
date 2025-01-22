d16857da96d099c5e8cc3625bdaf76c2a369a66a
Duc <thienductranhuu2784@gmail.com>
2025-01-22 00:07:07 -0500

    set up view engine jsp, test by passing data to view from model-controller

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -5,16 +5,26 @@ import org.springframework.web.bind.annotation.RestController;
 import vn.hoidanit.laptopshop.services.UserService;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //MVC pattern
 @Controller
 public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @RequestMapping("/")
-    public String getHomePage() {
-        return "duc.html";
+    public String getHomePage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("eric", test);
+        model.addAttribute("hoidanit", "controller with model");
+        // return this.userService.handleHello();
+        return "hello";
     }
 }
 // @RestController
