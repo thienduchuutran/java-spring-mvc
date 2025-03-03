6c1f96a7451f86ce049e003fca29113928e06092
Duc <thienductranhuu2784@gmail.com>
2025-03-03 17:52:43 -0500

    complete file upload feature when creating new user followed MVC model

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.multipart.MultipartFile;
 import jakarta.servlet.ServletContext;
 import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.repository.UserRepository;
+import vn.hoidanit.laptopshop.services.UploadService;
 import vn.hoidanit.laptopshop.services.UserService;
 
 import java.io.BufferedOutputStream;
@@ -28,11 +29,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class UserController {
     private final UserService userService;
-    private final ServletContext servletContext;
+    private final UploadService uploadService;
 
-    public UserController(UserService userService, ServletContext servletContext) {
+    public UserController(UserService userService, UploadService uploadService) {
         this.userService = userService;
-        this.servletContext = servletContext;
+        this.uploadService = uploadService;
     }
 
     @RequestMapping("/")
@@ -89,29 +90,7 @@ public class UserController {
             @ModelAttribute("newUser") User hoidanit,
             @RequestParam("hoidanitFile") MultipartFile file) { // getting value of user in
                                                                 // view to save in db
-
-        try {
-            byte[] bytes;
-            bytes = file.getBytes();
-            String rootPath = this.servletContext.getRealPath("/resources/images");
-
-            File dir = new File(rootPath + File.separator + "avatar");
-            if (!dir.exists())
-                dir.mkdirs();
-
-            // Create the file on server
-            File serverFile = new File(dir.getAbsolutePath() + File.separator +
-                    +System.currentTimeMillis() + "-" + file.getOriginalFilename());
-
-            BufferedOutputStream stream = new BufferedOutputStream(
-                    new FileOutputStream(serverFile));
-            stream.write(bytes);
-            stream.close();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
+        this.uploadService.handleSaveUploadFile(file, "avatar")
         this.userService.handleSaveUser(hoidanit);
         return "redirect:/admin/user";
     }
---
+++ src/main/java/vn/hoidanit/laptopshop/services/UploadService.java
@@ -0,0 +1,46 @@
+package vn.hoidanit.laptopshop.services;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.servlet.ServletContext;
+
+@Service
+public class UploadService {
+    private final ServletContext servletContext;
+
+    public UploadService(ServletContext servletContext) {
+        this.servletContext = servletContext;
+    }
+
+    public void handleSaveUploadFile(MultipartFile file, String targetFolder) {
+        String rootPath = this.servletContext.getRealPath("/resources/images");
+
+        try {
+            byte[] bytes;
+            bytes = file.getBytes();
+
+            File dir = new File(rootPath + File.separator + targetFolder); // dynamically save the file to whever we
+                                                                           // want
+            if (!dir.exists())
+                dir.mkdirs();
+
+            // Create the file on server
+            File serverFile = new File(dir.getAbsolutePath() + File.separator +
+                    +System.currentTimeMillis() + "-" + file.getOriginalFilename());
+
+            BufferedOutputStream stream = new BufferedOutputStream(
+                    new FileOutputStream(serverFile));
+            stream.write(bytes);
+            stream.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
